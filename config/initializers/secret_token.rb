4304b94329d4585c66d4d006f9c60e05b2ab715c
rick_barkley <rick_barkley@nuvola-networks.com>
2013-08-21 11:45:10 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuvolaAcademy::Application.config.secret_token = '4b3c576947a4868f574fd4cab31d4f06883d57babb229f747bd4369a5e07edf3a366de2deaeb10fe81a9f4dbe4ac8eaab8e7305150f103f24b0c7c9433ad4b30'
