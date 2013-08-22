22f4e242a5916464da5c463c40cd80ad86fc8a06
rick_barkley <rick_barkley@nuvola-networks.com>
2013-08-22 00:57:43 -0500

    added stripe payment module and order setup

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby '1.9.3'
 gem 'rails', '3.2.13'
 gem 'sqlite3'
 gem 'heroku'
+gem 'stripe'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,83 +1,27 @@
 class OrdersController < ApplicationController
-  # GET /orders
-  # GET /orders.json
-  def index
-    @orders = Order.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @orders }
+    def new
+        @course = Course.find(params[:course_id])
+        @order = @course.orders.new
+        
     end
-  end
-
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-    @order = Order.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @order }
+    def show
+        #@course = Course.find(params[:id])
+        @order = Order.find(params[:id])
     end
-  end
-
-  # GET /orders/new
-  # GET /orders/new.json
-  def new
-    @order = Order.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @order }
+    def index
+        #@user = User.find(params[:id])
+    	@orders = Orders.all
+        @course = Course.all
+        @title = "Available Classes"
     end
-  end
-
-  # GET /orders/1/edit
-  def edit
-    @order = Order.find(params[:id])
-  end
-
-  # POST /orders
-  # POST /orders.json
-  def create
-    @order = Order.new(params[:order])
-
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render json: @order, status: :created, location: @order }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+    
+    def create
+        
+        @order = Order.new(params[:order])
+        if @order.save_with_payment
+            redirect_to @order, :notice => "Thank you for your purchase!  Enjoy your Class!"
+            else
+            render :new
+        end
     end
-  end
-
-  # PUT /orders/1
-  # PUT /orders/1.json
-  def update
-    @order = Order.find(params[:id])
-
-    respond_to do |format|
-      if @order.update_attributes(params[:order])
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order = Order.find(params[:id])
-    @order.destroy
-
-    respond_to do |format|
-      format.html { redirect_to orders_url }
-      format.json { head :no_content }
-    end
-  end
 end
