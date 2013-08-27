e3bfa416a7a551fa9ada38e2d13d2b8769c547a1
rick_barkley <rick_barkley@nuvola-networks.com>
2013-08-26 19:09:19 -0500

    fixed course view and purchase links

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,11 +13,11 @@ class Course < ActiveRecord::Base
         !orders.blank? # Returns true if there is at least one order found, otherwise returns false
     end
     
-    def accessible_to_user?(user)
+    #def accessible_to_user?(user)
         # Look for course orders that belong to given user and have not expired
-        enrolls = self.enrolls.where("user_id = ?", user.id)
-        !enrolls.blank? # Returns true if there is at least one order found, otherwise returns false
-        end
+    #   enrolls = self.enrolls.where("user_id = ?", user.id)
+    #   !enrolls.blank? # Returns true if there is at least one order found, #otherwise returns false
+    #end
     
     validates :title,   	:presence   => true,
     :length     => { :maximum => 50 }
