170abe94f92ca0ed484909d185490b2dfaf9614a
rick_barkley <rick_barkley@nuvola-networks.com>
2013-08-28 11:31:03 -0500

    updated enroll method of adding students, it is now working

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,14 +10,16 @@ class Course < ActiveRecord::Base
     def accessible_by_user?(user)
         # Look for course orders that belong to given user and have not expired
         orders = self.orders.where("user_id = ?", user.id)
+        #enrolls = self.enrolls.where("user_id = ?", user.id)
         !orders.blank? # Returns true if there is at least one order found, otherwise returns false
+        #!enrolls.blank?
     end
     
-    #def accessible_to_user?(user)
-        # Look for course orders that belong to given user and have not expired
-    #   enrolls = self.enrolls.where("user_id = ?", user.id)
-    #   !enrolls.blank? # Returns true if there is at least one order found, #otherwise returns false
-    #end
+    def accessible_to_user?(user)
+        #Look for course orders that belong to given user and have not expired
+      enrolls = self.enrolls.where("user_id = ?", user.id)
+       !enrolls.blank? # Returns true if there is at least one order found, #otherwise returns false
+    end
     
     validates :title,   	:presence   => true,
     :length     => { :maximum => 50 }
