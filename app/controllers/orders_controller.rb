212937b0d8e8a5a0934a4647f9aed01748c71638
rick_barkley <rick_barkley@nuvola-networks.com>
2013-08-30 09:45:14 -0500

    fixed orders controller to allow destroy and update actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,4 +24,39 @@ class OrdersController < ApplicationController
             render :new
         end
     end
+    
+    # GET /courses/1/edit
+    def edit
+        @order = Order.find(params[:id])
+    end
+    
+    # PUT /courses/1
+    # PUT /courses/1.json
+    def update
+        @order = Order.find(params[:id])
+        
+        respond_to do |format|
+            if @order.update_attributes(params[:order])
+                format.html { redirect_to @order, notice: 'Course was successfully updated.' }
+                format.json { head :no_content }
+                else
+                format.html { render action: "edit" }
+                format.json { render json: @order.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    # DELETE /courses/1
+    # DELETE /courses/1.json
+    def destroy
+        @order = Order.find(params[:id])
+        @order.destroy
+        
+        respond_to do |format|
+            format.html { redirect_to orders_url }
+            format.json { head :no_content }
+        end
+    end
 end
+
+
